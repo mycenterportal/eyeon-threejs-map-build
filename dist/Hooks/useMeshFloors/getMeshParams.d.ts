88c671b65390fc4af375ea06fdaa5d8a24e71b71
Angrej Kumar <angrej.kumar@gmail.com>
2023-12-23 04:32:22 +0530

    Build: 0.0.13

--- dist/Hooks/useMeshFloors/getMeshParams.d.ts
+++ dist/Hooks/useMeshFloors/getMeshParams.d.ts
@@ -3,7 +3,7 @@ import { Graph } from "ngraph.graph";
 import { Color } from "three";
 import { IConfig, IFloorData } from "../../types";
 export declare const getMeshParams: (path: SVGResultPaths, paths: SVGResultPaths[], floors: IFloorData[], floor_index: number, config: IConfig, nodeCount: number, allNodesFloor: Record<string, number>, pathFinderGraph: Graph) => {
-    mesh_type: "base" | "amenity" | "kiosk" | "route-path" | "escalator" | "overlay" | "building-base" | "store" | "big-store" | "wall" | "outer-wall" | "boundary" | "special-shape" | null;
+    mesh_type: "amenity" | "kiosk" | "route-path" | "escalator" | "overlay" | "building-base" | "base" | "store" | "big-store" | "wall" | "outer-wall" | "boundary" | "special-shape" | null;
     layer_color: Color;
     extrude: number;
     z_index: number;
