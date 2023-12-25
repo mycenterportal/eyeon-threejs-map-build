aa65f80a43268a209423dd8ce34e46a5ab29b2ae
EyeOn Github <github@eyeonllc.com>
2023-12-25 12:20:03 +0000

    Build: 7bbc6f69cafe384f130485bf34a3855976d81fc1

---
+++ dist/components/UIComponent/FloorSelector.d.ts
@@ -0,0 +1,3 @@
+import { IFloorSelectorProps } from "../../types";
+declare const FloorSelector: ({ floors, selectedFloorIndex, handleFloorChange, accentColor }: IFloorSelectorProps) => import("react/jsx-runtime").JSX.Element;
+export default FloorSelector;
