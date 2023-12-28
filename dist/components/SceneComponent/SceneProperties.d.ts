86721634df60ecf3a5e3bb906543bd58fa460e25
EyeOn Github <github@eyeonllc.com>
2023-12-28 23:34:49 +0000

    Build: 0.0.28

---
+++ dist/components/SceneComponent/SceneProperties.d.ts
@@ -0,0 +1,6 @@
+import { Color } from "three";
+interface IScenePropertiesProps {
+    background?: Color | string;
+}
+export declare const SceneProperties: (params: IScenePropertiesProps) => null;
+export {};
