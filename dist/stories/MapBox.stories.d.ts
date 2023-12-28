666cd7d51f081b9d48d7cca7f994b056428ee9f4
EyeOn Github <github@eyeonllc.com>
2023-12-28 16:22:24 +0000

    Build: 0.0.26

---
+++ dist/stories/MapBox.stories.d.ts
@@ -0,0 +1,39 @@
+import type { StoryObj } from '@storybook/react';
+import MapBox from '../MapBox';
+declare const meta: {
+    title: string;
+    component: typeof MapBox;
+    parameters: {
+        layout: string;
+    };
+    tags: string[];
+    argTypes: {
+        config: {
+            control: string;
+            description: string;
+        };
+        stats: {
+            control: string;
+            description: string;
+        };
+        mapitData: {
+            control: string;
+            description: string;
+        };
+    };
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+export declare const Editor35: Story;
+export declare const Editor33: Story;
+export declare const Editor80: Story;
+export declare const Editor81: Story;
+export declare const Viewer81: Story;
+export declare const WPSite81: Story;
+export declare const Editor83: Story;
+export declare const Viewer83: Story;
+export declare const Viewer33: Story;
+export declare const Editor8: Story;
+export declare const Viewer: Story;
+export declare const WidthDefaultSelected: Story;
+export declare const YorkTownCenter: Story;
