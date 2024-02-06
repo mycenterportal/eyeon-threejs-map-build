dc908453d691d0e4de2ca748283f9824eb761213
EyeOn Github <github@eyeonllc.com>
2024-02-06 08:34:47 +0000

    Build: 0.0.51

---
+++ dist/components/SceneComponent/SceneComponent.d.ts
@@ -0,0 +1,26 @@
+import React from "react";
+import { IJsonConfig } from "src/types";
+import { MapIt2Response, MapObj, MapObjToSave } from "../../mapitApiTypes";
+interface ISceneComponentProps {
+    mapitData?: MapIt2Response;
+    config: Partial<IJsonConfig>;
+    selectedActiveObjectId: string;
+    setSelectedActiveObjectId: React.Dispatch<React.SetStateAction<string>>;
+    onMapDataUpdate?: (data: MapObj[]) => void;
+    onSettingsLoading?: (settings: MapIt2Response) => void;
+    webApiURI?: string;
+    mediaStorageURI?: string;
+    onSubmit?: (data: MapObjToSave, refreshData?: () => void) => void;
+    onResetData?: (data: MapObj) => void;
+    deviceType?: string | "";
+    setCameraControlPosition?: (cameraControlPos: any) => void;
+}
+export interface IZoomData {
+    direction: 'in' | 'out';
+}
+export type TFormMapObjData = {
+    index: number;
+    data: MapObj;
+};
+declare const SceneComponent: React.ForwardRefExoticComponent<ISceneComponentProps & React.RefAttributes<unknown>>;
+export default SceneComponent;
