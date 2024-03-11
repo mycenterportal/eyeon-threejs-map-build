173e2d03fc174058ee3503c4e0fc73fcda8f7bff
EyeOn Github <github@eyeonllc.com>
2024-03-11 17:01:14 +0000

    Build: 0.0.75

---
+++ dist/components/SceneComponent/FloorsMap.d.ts
@@ -0,0 +1,47 @@
+import { IConfig, IMeshParams } from "../../types";
+import React, { MouseEventHandler } from "react";
+import { Mesh } from "three";
+import { ThreeEvent } from "@react-three/fiber";
+import { IZoomData } from "./SceneComponent";
+interface IFloorsMapProps {
+    meshFloors: IMeshParams;
+    currentFloorIndex: number;
+    onPointerEnter?: (e: ThreeEvent<PointerEvent>) => void;
+    onPointerLeave?: (e: ThreeEvent<PointerEvent>) => void;
+    onPointerMove?: (e: ThreeEvent<PointerEvent>) => void;
+    onClick?: (e: ThreeEvent<MouseEvent>) => void;
+    onCameraMove?: (e: any) => void;
+    currKioskObj: Mesh;
+    currKioskFloorIndex: number;
+    routeTargetId?: string;
+    routeTargetFromId?: string;
+    amenityTargetType: string;
+    activeObjectId?: string;
+    hoverObjectId?: string;
+    handleChangeFloor: (index: number) => MouseEventHandler<HTMLDivElement>;
+    escalatorNodes: string[];
+    zoom: IZoomData | null;
+    handleCameraLength?: (length: number) => void;
+    config: IConfig;
+    cameraPos?: {
+        x: number;
+        y: number;
+        z: number;
+    } | undefined | null;
+    controlPos?: {
+        x: number;
+        y: number;
+        z: number;
+    } | undefined | null;
+    handleCameraAndControlsChange?: (camPos: {
+        x: number;
+        y: number;
+        z: number;
+    }, ctrlTarget: {
+        x: number;
+        y: number;
+        z: number;
+    }) => void;
+}
+export declare const FloorsMap: React.ForwardRefExoticComponent<IFloorsMapProps & React.RefAttributes<unknown>>;
+export {};
