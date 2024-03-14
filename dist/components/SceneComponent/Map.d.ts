c96e39eee334cd350343ea7ea099c8442ad1a509
EyeOn Github <github@eyeonllc.com>
2024-03-14 20:31:41 +0000

    Build: 0.0.83

---
+++ dist/components/SceneComponent/Map.d.ts
@@ -0,0 +1,19 @@
+import { IMeshParams } from "../../types";
+import { ThreeEvent } from "@react-three/fiber";
+import { Mesh } from "three";
+interface IMapProps {
+    floorIndex: number;
+    meshFloors: IMeshParams;
+    currKioskObj: Mesh | null;
+    activeObjectId?: string;
+    hoverObjectId?: string;
+    routeTube?: Mesh;
+    visible: boolean;
+    onPointerEnter?: (e: ThreeEvent<PointerEvent>) => void;
+    onPointerLeave?: (e: ThreeEvent<PointerEvent>) => void;
+    onPointerMove?: (e: ThreeEvent<PointerEvent>) => void;
+    onClick?: (e: ThreeEvent<MouseEvent>) => void;
+    handleChangeFloor: (index: number) => void;
+}
+export declare const Map: (params: IMapProps) => import("react/jsx-runtime").JSX.Element;
+export {};
