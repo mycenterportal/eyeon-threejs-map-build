76bc3e2c68e4a3acfd3654c0740daccdb322065a
EyeOn Github <github@eyeonllc.com>
2024-04-09 18:17:11 +0000

    Build: 0.0.97

---
+++ dist/defaults.d.ts
@@ -0,0 +1,3 @@
+import { IJsonConfig } from "./types";
+export declare const mapit2DefaultVars: Partial<IJsonConfig>;
+export declare const defaultVars: IJsonConfig;
---
+++ dist/stories/MapBox.stories.d.ts
@@ -0,0 +1,49 @@
+import type { StoryObj } from "@storybook/react";
+import MapBox from "../MapBox";
+declare const BasicTab: {
+    title: string;
+    component: typeof MapBox;
+    parameters: {
+        layout: string;
+    };
+    tags: string[];
+    argTypes: {
+        config: {
+            control: string;
+            description: string;
+        };
+        stats: {
+            control: string;
+            description: string;
+        };
+        mapitData: {
+            control: string;
+            description: string;
+        };
+    };
+};
+export default BasicTab;
+type BasicTabStory = StoryObj<typeof BasicTab>;
+export declare const Editor35: BasicTabStory;
+export declare const Editor33: BasicTabStory;
+export declare const Editor80: BasicTabStory;
+export declare const Editor81: BasicTabStory;
+export declare const Viewer81: BasicTabStory;
+export declare const WPSite81: BasicTabStory;
+export declare const Editor83: BasicTabStory;
+export declare const Viewer83: BasicTabStory;
+export declare const Editor84: BasicTabStory;
+export declare const Viewer84: BasicTabStory;
+export declare const DisplayApp84: BasicTabStory;
+export declare const Viewer33: BasicTabStory;
+export declare const RESPONSIVE: BasicTabStory;
+export declare const Editor8: BasicTabStory;
+export declare const Viewer: BasicTabStory;
+export declare const WidthDefaultSelected: BasicTabStory;
+export declare const YorkTownCenter: BasicTabStory;
+export declare const BridgeWaterCommons: BasicTabStory;
+export declare const EverGreenMall: BasicTabStory;
+export declare const EverGreenMallWP: BasicTabStory;
+export declare const EverGreenMallApple: BasicTabStory;
+export declare const Editor84Responsive: BasicTabStory;
+export declare const CentersSelector: BasicTabStory;
