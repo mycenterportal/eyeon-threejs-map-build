47d748f0efcaed8633e13c8739244c9e9d824113
EyeOn Github <github@eyeonllc.com>
2024-04-10 17:49:01 +0000

    Build: 0.0.98

---
+++ dist/useMapit2Data.d.ts
@@ -0,0 +1,18 @@
+import { MapIt2Response } from "./mapitApiTypes";
+interface useMapIt2DataProps {
+    CENTER_ID: string;
+    mapitData?: MapIt2Response;
+    webApiURI?: string;
+}
+type Mapit2DataReturn = {
+    data: MapIt2Response | null;
+    refreshData: () => void;
+    removeMissingMapObjects: (objectNames: string[]) => void;
+};
+/**
+ * This is a hook that fetches the mapit2 data from the server by CENTER_ID. or uses the data passed in.
+ * @param CENTER_ID
+ * @param mapitData
+ */
+export declare function useMapit2Data({ CENTER_ID, mapitData, webApiURI }: useMapIt2DataProps): Mapit2DataReturn;
+export {};
