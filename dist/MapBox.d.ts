70132fc444e2f22415112c8a6f9b8c696532e377
EyeOn Github <github@eyeonllc.com>
2024-04-12 19:04:46 +0000

    Build: 0.0.106

---
+++ dist/MapBox.d.ts
@@ -0,0 +1,26 @@
+import { MutableRefObject } from 'react';
+import { MapIt2Response, MapObj, MapObjToSave } from "./mapitApiTypes";
+import { IJsonConfig } from "./types";
+import 'bootstrap/dist/css/bootstrap.min.css';
+export type MapBoxRefFs = {
+    refreshData: () => void;
+    createRouteToAmenity: (amenityId: AmenityID) => void;
+    createRouteToStore: (retailerSlug: string) => void;
+};
+export type AmenityID = 'atm' | 'management' | 'playarea' | 'restroom' | 'family-restroom' | 'elevator' | 'security' | 'dog-poop-station';
+export interface IAppProps {
+    mapitData?: unknown;
+    config: Partial<IJsonConfig>;
+    stats?: boolean;
+    onSettingsLoading?: (settings: MapIt2Response) => void;
+    webApiURI?: string;
+    mediaStorageURI?: string;
+    onSubmit?: (data: MapObjToSave, refreshData?: () => void) => void;
+    refObj?: MutableRefObject<MapBoxRefFs | null> | undefined;
+    onResetData?: (data: MapObj) => void;
+    deviceType?: string | "";
+    setCameraControlPosition?: (cameraControlPos: any) => void;
+    onExtractedAmenities?: (amenityIds: AmenityID[]) => void;
+}
+declare function MapBox({ mapitData, config, onSettingsLoading, webApiURI, mediaStorageURI, onSubmit, refObj, deviceType, setCameraControlPosition, onResetData, onExtractedAmenities }: IAppProps): import("react/jsx-runtime").JSX.Element | null;
+export default MapBox;
---
+++ dist/helpers/misc.d.ts
@@ -0,0 +1,3 @@
+import { Color } from "three";
+export declare function hex_to_color(hex_code: string): Color;
+export declare const getFormatedColor: (color: string) => string;
