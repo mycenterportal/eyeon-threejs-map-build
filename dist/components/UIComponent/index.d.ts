e493d7ca145d39cc4501f75517a0a835197a506c
EyeOn Github <github@eyeonllc.com>
2024-04-17 19:13:57 +0000

    Build: 0.0.109

---
+++ dist/components/UIComponent/index.d.ts
@@ -0,0 +1,16 @@
+import { IAmenitiesInteractiveList, IFloorSelectorProps, TRoles } from "src/types";
+import { MouseEventHandler } from "react";
+import { MapObj } from "src/mapitApiTypes";
+import { AmenityID } from "src/MapBox";
+declare const UIComponent: ({ floors, accentColor, selectedFloorIndex, handleFloorChange, amenitiesList, zoomIn, zoomOut, reset, clearRoute, handleAmenityClick, role, mapObjs, onExtractedAmenities, }: IFloorSelectorProps & {
+    amenitiesList: IAmenitiesInteractiveList[];
+    zoomIn?: MouseEventHandler<HTMLDivElement> | undefined;
+    zoomOut?: MouseEventHandler<HTMLDivElement> | undefined;
+    reset?: ((fullReset?: boolean) => void) | undefined;
+    clearRoute?: MouseEventHandler<HTMLDivElement> | undefined;
+    handleAmenityClick: (type: string) => void;
+    role?: TRoles | undefined;
+    mapObjs?: MapObj[] | undefined;
+    onExtractedAmenities?: ((amenityIds: AmenityID[]) => void) | undefined;
+}) => import("react/jsx-runtime").JSX.Element;
+export default UIComponent;
