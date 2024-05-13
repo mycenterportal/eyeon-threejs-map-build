415f1d675d171a6a28ec4257f25be02c049bad12
EyeOn Github <github@eyeonllc.com>
2024-05-13 22:11:55 +0000

    Build: 0.0.134

---
+++ dist/components/SceneComponent/RouteTube.d.ts
@@ -0,0 +1,6 @@
+import type { Mesh } from "three";
+interface IRouteTubeProps {
+    mesh?: Mesh;
+}
+export declare function RouteTube({ mesh }: IRouteTubeProps): import("react/jsx-runtime").JSX.Element | null;
+export {};
