982fcf2ffdfa1f1deed1475fe686c6bc04787c01
EyeOn Github <github@eyeonllc.com>
2024-05-22 18:10:10 +0000

    Build: 0.0.137

---
+++ dist/helpers/misc.d.ts
@@ -0,0 +1,4 @@
+import { Color } from "three";
+export declare function hex_to_color(hex_code: string): Color;
+export declare const getFormatedColor: (color: string) => string;
+export declare const getSvgIconHTML: (icon: string) => string | undefined;
---
+++ dist/services/retailersApi.d.ts
@@ -0,0 +1,29 @@
+import { IRetailer } from 'src/mapitApiTypes';
+export declare const retailersApi: import("@reduxjs/toolkit/query/react").Api<import("@reduxjs/toolkit/query/react").BaseQueryFn<string | import("@reduxjs/toolkit/query/react").FetchArgs, unknown, import("@reduxjs/toolkit/query/react").FetchBaseQueryError, {}, import("@reduxjs/toolkit/query/react").FetchBaseQueryMeta>, {
+    getRetailers: import("@reduxjs/toolkit/query/react").QueryDefinition<{
+        baseQuery: string;
+        center_id: string;
+        limit: number;
+        page?: number | undefined;
+    }, import("@reduxjs/toolkit/query/react").BaseQueryFn<string | import("@reduxjs/toolkit/query/react").FetchArgs, unknown, import("@reduxjs/toolkit/query/react").FetchBaseQueryError, {}, import("@reduxjs/toolkit/query/react").FetchBaseQueryMeta>, never, {
+        count: number;
+        items: IRetailer[];
+    }, "retailersApi">;
+}, "retailersApi", never, typeof import("@reduxjs/toolkit/query/react").coreModuleName | typeof import("@reduxjs/toolkit/query/react").reactHooksModuleName>;
+export declare const useGetRetailersQuery: import("@reduxjs/toolkit/dist/query/react/buildHooks").UseQuery<import("@reduxjs/toolkit/query/react").QueryDefinition<{
+    baseQuery: string;
+    center_id: string;
+    limit: number;
+    page?: number | undefined;
+}, import("@reduxjs/toolkit/query/react").BaseQueryFn<string | import("@reduxjs/toolkit/query/react").FetchArgs, unknown, import("@reduxjs/toolkit/query/react").FetchBaseQueryError, {}, import("@reduxjs/toolkit/query/react").FetchBaseQueryMeta>, never, {
+    count: number;
+    items: IRetailer[];
+}, "retailersApi">>, useLazyGetRetailersQuery: import("@reduxjs/toolkit/dist/query/react/buildHooks").UseLazyQuery<import("@reduxjs/toolkit/query/react").QueryDefinition<{
+    baseQuery: string;
+    center_id: string;
+    limit: number;
+    page?: number | undefined;
+}, import("@reduxjs/toolkit/query/react").BaseQueryFn<string | import("@reduxjs/toolkit/query/react").FetchArgs, unknown, import("@reduxjs/toolkit/query/react").FetchBaseQueryError, {}, import("@reduxjs/toolkit/query/react").FetchBaseQueryMeta>, never, {
+    count: number;
+    items: IRetailer[];
+}, "retailersApi">>;
