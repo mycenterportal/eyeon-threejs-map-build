a689e7fc8b7bc62da90cb6e8ce3c52ab12a0e865
EyeOn Github <github@eyeonllc.com>
2025-10-10 19:43:04 +0000

    Build: 1.0.5

--- dist/types/style-modules.d.ts
+++ dist/types/style-modules.d.ts
@@ -1,9 +1,4 @@
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.scss';
+// removed scss declarations
 
 declare module '*.module.css' {
   const classes: { [key: string]: string };
