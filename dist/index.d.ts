63b35c5e97e295e5ffe3dc0b0156deb0460da866
EyeOn Github <github@eyeonllc.com>
2026-01-02 08:30:41 +0000

    Build: 1.1.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,6 @@
 import { default as MapBox, MapBoxRefProps, AppProps } from './MapBox';
-export type { MapConfigProps } from './interfaces';
 export { MapBox };
 export type { MapBoxRefProps, AppProps };
+export type { MapConfigProps, MapDeviceType } from './interfaces';
+export type { MapCameraControlsState, MapResponsiveSettings, FloorMapResponsiveSettings, MapAllDataResponse, MapObjData, MapVectorPoint } from './interfaces/mapApiTypes';
+export type { PortalMapSettingsProps } from './interfaces/portal';
