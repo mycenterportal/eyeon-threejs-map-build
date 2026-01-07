bac419abfa1be3ad3fda02285fff2a79f505deda
EyeOn Github <github@eyeonllc.com>
2026-01-07 19:39:47 +0000

    Build: 1.1.10

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,3 +4,4 @@ export type { MapBoxRefProps, AppProps };
 export type { MapConfigProps, MapDeviceType } from './interfaces';
 export type { MapCameraControlsState, MapResponsiveSettings, FloorMapResponsiveSettings, MapAllDataResponse, MapObjData, MapVectorPoint } from './interfaces/mapApiTypes';
 export type { PortalMapSettingsProps } from './interfaces/portal';
+export type { MapAmenityID } from './helpers/amenities.helper';
